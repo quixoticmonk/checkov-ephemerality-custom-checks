3e50e8655a47e9f0d069d5cb3e787e40c6dd0f78
quixoticmonk <manuchandrasekhar@gmail.com>
2025-06-19 20:59:31 -0400

    feat: added kubernetes provider

--- generators/ephemeral_resources/providers.tf
+++ generators/ephemeral_resources/providers.tf
@@ -2,28 +2,32 @@ terraform {
   required_providers {
     # Please add new providers to this list as desired
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
     azuread = {
-      source  = "hashicorp/azuread"
+      source = "hashicorp/azuread"
     }
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
     }
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
     }
     tfe = {
-      source  = "hashicorp/tfe"
+      source = "hashicorp/tfe"
     }
     tls = {
-      source  = "hashicorp/tls"
+      source = "hashicorp/tls"
     }
     random = {
-      source  = "hashicorp/random"
+      source = "hashicorp/random"
     }
     vault = {
-      source  = "hashicorp/vault"
+      source = "hashicorp/vault"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+
     }
   }
 }
